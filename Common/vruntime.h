c11c22572c221f5a97b31048156d62739dc388a9
psychokinesis <psychokinesis@8608462f-0e06-4f73-9daa-bc286878d562>
2011-10-09 21:06:46 +0000

    added sol looping condition helper functions

--- Common/vruntime.h
+++ Common/vruntime.h
@@ -496,6 +496,73 @@ inline void CRunObject::UpdateBoundingBox()
 	pRuntime->UpdateBoundingBox(this);
 }
 
+// Include StdAfx
+#include "StdAfx.h"
+
+//functions for iterating through a list of objects, and picking them one at a time
+//be sure to add the SOL_MODIFIER flag in the ACETABLE:
+//ADDCND("My condition", "My category", "%o My condition", &ExtObject::cMyCondition, "MyCondition", SOL_MODIFIER);
+//example:
+//long ExtObject::cBareMinimumLoopingCondition(LPVAL params)
+//{
+//	ExtObject::StartSOLLoopCondition();
+//	for(yourloopingcondition)
+//	{
+//		StartSOLLoopIteration();
+//		//Your code that will run on that iteration immediately prior to running the actions and subconditions of this condition
+//		//Example:
+//		//this makes it so all (or no) instances are picked, you do this to clear the picking of that object type:
+//		//pRuntime->SelectAll(yourObjectType);
+//		//this picks one instance(obj) of an object type(pType): SelectF(CRunObject* obj, CRunObjType* pType)
+//		//pRuntime->SelectF(yourObjectInstances[i],yourObjectType);
+//
+//		
+//		if(EndSOLLoopIteration())
+//			break;
+//	}
+//		EndSOLLoopCondition();//forgetting this will cause a memory leak
+//	// always on looping SOL conditions:
+//	return false;
+//		
+//} 
+void CRunObject::StartSOLLoopCondition()
+{
+	EventParametersInfo2 newepi;
+	epis.push_back(newepi);
+     pRuntime->GetCurrentEventPtrs(epis.back());
+}
+
+void CRunObject::StartSOLLoopIteration()
+{
+     pRuntime->NewSOLCopy();
+}
+
+bool CRunObject::EndSOLLoopIteration()
+{
+     if (epis.back().pCnds && !pRuntime->RunConditions(epis.back().pCnds))
+	 {
+          pRuntime->RestoreSOL();
+          return true;
+     }
+
+
+     if (epis.back().pActs)
+          pRuntime->RunActions(epis.back().pActs);
+
+     if (epis.back().pSubEvents)
+          pRuntime->RunSubEvents(epis.back().pSubEvents);
+
+     pRuntime->RestoreSOL();
+     return false;
+}
+
+void CRunObject::EndSOLLoopCondition()
+{
+	
+	epis.pop_back();
+
+}
+
 inline void CRunObject::TriggerEvent(int cndIndex)
 {
 	pRuntime->GenerateEvent(info.oid, cndIndex, this);
